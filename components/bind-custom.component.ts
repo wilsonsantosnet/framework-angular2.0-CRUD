fa6efd5964af0014b871f8cf8c11f91e3d8b4cf1
Wilson Santos <wilsonsantosnet@gmail.com>
2017-08-28 10:02:51 -0300

    Add Sanitize Dom Check / Converte

--- components/bind-custom.component.ts
+++ components/bind-custom.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, SecurityContext  } from '@angular/core';
 import { DatePipe, DecimalPipe, PercentPipe, CurrencyPipe } from "@angular/common";
+import { DomSanitizer } from '@angular/platform-browser';
 import { ApiService } from "app/common/services/api.service";
 
 @Component({
@@ -9,11 +10,12 @@ import { ApiService } from "app/common/services/api.service";
       <label *ngIf="tag === 'label'">{{ value }}</label>
       <p *ngIf="tag === 'p'">{{ value }}</p>
       <div *ngIf="tag === 'div'">{{ value }}</div>
+      <div *ngIf="tag === 'inner'" [innerHTML]='value'></div>
     `,
     providers: [DatePipe, DecimalPipe, PercentPipe, CurrencyPipe, ApiService],
 })
 export class BindCustomComponent implements OnInit, OnChanges {
-    
+
     value: any;
     badge: boolean;
 
@@ -25,58 +27,69 @@ export class BindCustomComponent implements OnInit, OnChanges {
     @Input() key: string;
     @Input() aux: any;
 
-    //datePipe: DatePipe;
+    datePipe: DatePipe;
 
     constructor(
         private decimalPipe: DecimalPipe,
         private percentPipe: PercentPipe,
         private currencyPipe: CurrencyPipe,
+        private sanitizer: DomSanitizer,
         private api: ApiService<any>) {
 
-        //this.datePipe = new DatePipe("pt-BR");
+        this.datePipe = new DatePipe("pt-BR");
+    }
+
+    public convertDate(value) {
+        if (value) {
+            var datePart = value.toString().split("/");
+            var convertedDate = datePart[1] + "/" + datePart[0] + "/" + datePart[2];
+            return new Date(convertedDate);
+        }
     }
 
     ngOnChanges(changes: SimpleChanges): void {
 
-        //console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-        //if (this.format.toLocaleLowerCase() === 'date')
-        //    this.value = this.datePipe.transform(this.model, 'dd/MM/yyyy');
+        if (this.format.toLocaleLowerCase() === 'date') {
+            this.value = this.datePipe.transform(this.convertDate(this.model), 'dd/MM/yyyy');
+        }
+        else if (this.format.toLocaleLowerCase() === 'time') {
+            this.value = this.datePipe.transform(this.convertDate(this.model), 'HH:mm');
+        }
+        else if (this.format.toLocaleLowerCase() === 'datetime' || this.format.toLocaleLowerCase() === 'datetime?') {
+            this.value = this.datePipe.transform(this.convertDate(this.model), 'dd/MM/yyyy HH:mm');
+        }
+        else if (this.format.toLocaleLowerCase() === 'decimal' && !isNaN(this.model)) {
+            this.value = this.decimalPipe.transform(this.model, '1.2-2');
+        }
+        else if ((this.format.toLocaleLowerCase() === 'integer' || this.format.toLocaleLowerCase() === 'int' || this.format.toLocaleLowerCase() === 'int?') && !isNaN(this.model)) {
+            this.value = this.decimalPipe.transform(this.model, '1.0-0');
+        }
+        else if (this.format.toLocaleLowerCase() === 'percent' && !isNaN(this.model)) {
+            this.value = this.percentPipe.transform(this.model, '1.2-2');
+        }
+        else if (this.format.toLocaleLowerCase() === 'currency' && !isNaN(this.model)) {
+            this.value = this.currencyPipe.transform(this.model, 'BRL', true, '1.2-2');
+        }
+        else if (this.format.toLocaleLowerCase() === 'instance') {
+            this._getInstance();
+        }
+        else if (this.format.toLocaleLowerCase() === 'dataitem') {
+            this.value = this._getInDataItem(this.model, this.aux);
+        }
+        else if (this.format.toLocaleLowerCase() === 'changevalue') {
+            this.value = this._getChangeForThis(this.model, this.aux);
+        }
+        else if (this.format.toLocaleLowerCase() === 'bool' || this.format.toLocaleLowerCase() === 'bool?') {
+            this.value = (this.model == true ? "Sim" : "Não"); this.badge = true;
+        }
+        else if (this.format.toLocaleLowerCase() === 'html') {
+            this.tag = "inner";
+            this.value = this.sanitizer.sanitize(SecurityContext.HTML, this.model);
+        }
+        else {
+            this.value = this.model;
+        }
 
-        //else if (this.format.toLocaleLowerCase() === 'time')
-        //    this.value = this.datePipe.transform(this.model, 'HH:mm');
-
-        //else if (this.format.toLocaleLowerCase() === 'datetime' || this.format.toLocaleLowerCase() === 'datetime?')
-        //     this.value = this.datePipe.transform(this.model, 'dd/MM/yyyy HH:mm');
-
-
-        if (this.format.toLocaleLowerCase() === 'decimal' && !isNaN(this.model))
-        { this.value = this.decimalPipe.transform(this.model, '1.2-2'); }
-
-        else if ((this.format.toLocaleLowerCase() === 'integer' ||
-            this.format.toLocaleLowerCase() === 'int' ||
-            this.format.toLocaleLowerCase() === 'int?') && !isNaN(this.model))
-        { this.value = this.decimalPipe.transform(this.model, '1.0-0'); }
-
-        else if (this.format.toLocaleLowerCase() === 'percent' && !isNaN(this.model))
-        { this.value = this.percentPipe.transform(this.model, '1.2-2'); }
-
-        else if (this.format.toLocaleLowerCase() === 'currency' && !isNaN(this.model))
-        { this.value = this.currencyPipe.transform(this.model, 'BRL', true, '1.2-2'); }
-
-        else if (this.format.toLocaleLowerCase() === 'instance')
-        { this._getInstance(); }
-
-        else if (this.format.toLocaleLowerCase() === 'dataitem')
-        { this.value = this._getInDataItem(this.model, this.aux); }
-
-        else if (this.format.toLocaleLowerCase() === 'changevalue')
-        { this.value = this._getChangeForThis(this.model, this.aux); }
-
-        else if (this.format.toLocaleLowerCase() === 'bool' || this.format.toLocaleLowerCase() === 'bool?')
-        { this.value = (this.model == true ? "Sim" : "Não"); this.badge = true; }
-
-        else
-        { this.value = this.model; }
     }
 
     ngOnInit(): void { }
