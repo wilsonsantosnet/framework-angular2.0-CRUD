d230a148ba30e4b10d2551ee1720730be4b21ded
Bruno Henrique D. Seco <bruuno14@msn.com>
2018-01-17 11:52:16 -0200

    Update cep.component.ts

--- components/cep.component.ts
+++ components/cep.component.ts
@@ -1,16 +1,16 @@
-﻿import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { ApiService } from '../services/api.service';
 import { GlobalService } from 'app/global.service';
 import { ViewModel } from '../model/viewmodel';
 
 @Component({
-	selector: 'cep',
-	template: `<div class="row" [formGroup]="vm.form">
+  selector: 'cep',
+  template: `<div class="row" [formGroup]="vm.form">
   <section class="col-md-2">
     <div class='form-group'>
       <label>{{ vm.infos.cep.label }}</label>
-      <input type='text' class='form-control'[(ngModel)]='vm.model.cep' name='cep'  formControlName='cep' [textMask]="{mask: vm.masks.maskCEP}" (change)="onChange(vm.model.cep)" required />      
+      <input type='text' class='form-control' [(ngModel)]='vm.model.cep' name='cep' cepFinder (onfind)="onFindCep($event)" formControlName='cep' [textMask]="{mask: vm.masks.maskCEP}" required />      
     </div>
   </section>
   <section class="col-md-6">
@@ -58,41 +58,27 @@ import { ViewModel } from '../model/viewmodel';
   </section>  
 </div>`
 })
-export class CepComponent  implements OnInit {
+export class CepComponent implements OnInit {
 
-	@Input() vm: ViewModel<any>
-	@Output() cepChange = new EventEmitter<any>();
+  @Input() vm: ViewModel<any>
+  @Output() cepChange = new EventEmitter<any>();
+  
+  constructor() {
+  }
 
-    _endpoint: string;
+  ngOnInit(): void {
 
-	constructor(private api: ApiService<any>) {
-		this._endpoint = "http://target-cep.azurewebsites.net/api/";
-    }
+  }
 
-    ngOnInit(): void {
+  onFindCep(data: any) {
 
-    }
+    this.vm.model.rua = data.logradouro;
+    this.vm.model.cidade = data.cidade;
+    this.vm.model.estado = data.uf;
+    this.vm.model.bairro = data.bairro;
 
-    onChange(cep: any) {
+    this.cepChange.emit(data);    
+  }
 
-		if (cep != null) {
-            this.api.setResource("log_logradouro/GetDataListCustom", this._endpoint).get({ cep: cep }).subscribe((result) => {
 
-				if (result.DataList == null || result.DataList.length < 1) {
-					result.DataList = [{
-						bairro: null, cep: null, cidade: null, logradouro: null, tipo: null, uf: null
-					}];
-				}
-
-				this.vm.model.rua = result.DataList[0].logradouro;
-				this.vm.model.cidade = result.DataList[0].cidade;
-				this.vm.model.estado = result.DataList[0].uf;
-				this.vm.model.bairro = result.DataList[0].bairro;
-
-				this.cepChange.emit(result.DataList[0]);
-			})
-		}
-	}
-
-	
 }
